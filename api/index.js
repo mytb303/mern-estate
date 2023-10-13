54ea49590d43645448198c1804184b3aec286bf9
mytb303 <mytb303@gmail.com>
2023-10-13 10:20:30 +0100

    connect to database after git error

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-<<<<<<< HEAD
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 //initialise dotenv
@@ -17,10 +16,6 @@ mongoose
 // create application and link to express after installing express
 const app = express();
 
-=======
-// create application and link to express after installing express
-const app = express();
->>>>>>> 805fc615008efec96adcb42d73eb3d1e73e73c1c
 // method to listen to port 3000 and in the callback function say server is running on port 3000
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
